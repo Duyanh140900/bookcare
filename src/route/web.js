8c055890dbc8c5812b1bae9742ff844d30a60915
Duyanh140900 <duymesut1@gmail.com>
2024-05-21 19:26:18 +0700

    first commit

---
+++ src/route/web.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { getHomePage, getAboutPage } from '../controllers/homeController';
+
+let router = express.Router();
+
+let initWebRoutes = (app) =>{
+    router.get('/', getHomePage)
+    router.get('/about', getAboutPage)
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes
